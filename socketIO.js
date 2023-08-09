755557f95f376bc3200741c6b010faa4038e6142
sinan <sinan@sinans-MacBook-Air.local>
2023-08-09 15:02:16 +0530

    updated the new URL,Hosted IP, the project is deployed is succesfully

--- socketIO.js
+++ socketIO.js
@@ -9,8 +9,8 @@ function socketConnection(server){
             // origin: "https://main.d3gzftg01ima7.amplifyapp.com",
             // origin: "https://etrainfrontend.onrender.com",
             
-            // origin: "https://main.d2p226zdkjksrz.amplifyapp.com",
-            origin: "http://localhost:3000",
+            origin: "https://main.d2p226zdkjksrz.amplifyapp.com",
+            // origin: "http://localhost:3000",
             methods:["GET","POST"],
         }
     })
